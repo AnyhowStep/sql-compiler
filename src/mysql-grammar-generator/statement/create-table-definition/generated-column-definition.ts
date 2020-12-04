caee1a980ff8e3a434d97d6a17c3e93eac2651af
AnyhowStep <5655961+AnyhowStep@users.noreply.github.com>
2020-12-04 07:39:18 -0500

    Attempt to add Travis CI

--- src/mysql-grammar-generator/statement/create-table-definition/generated-column-definition.ts
+++ src/mysql-grammar-generator/statement/create-table-definition/generated-column-definition.ts
@@ -12,6 +12,10 @@ makeCustomRule(SyntaxKind.ColumnDefinition)
         [
             SyntaxKind.ColumnIdentifier,
             CustomSyntaxKind.DataType,
+            /**
+             * @todo Allow `COLLATE` before `GeneratedDefinition`
+             * https://github.com/mysql/mysql-server/blob/5c8c085ba96d30d697d0baa54d67b102c232116b/sql/sql_yacc.yy#L6358
+             */
             SyntaxKind.GeneratedDefinition,
             CustomSyntaxKind.ColumnDefinitionModifier,
         ] as const,
