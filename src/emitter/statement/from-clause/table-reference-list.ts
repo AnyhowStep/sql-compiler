34e4d90fa6588c9e9de41c58028efeda9249e3c8
AnyhowStep <5655961+AnyhowStep@users.noreply.github.com>
2020-12-13 20:02:45 -0500

    Added table reference list tests with nested elements

--- src/emitter/statement/from-clause/table-reference-list.ts
+++ src/emitter/statement/from-clause/table-reference-list.ts
@@ -1,13 +1,29 @@
-import {TableReferenceList} from "../../../parser-node";
+import {SyntaxKind, TableReferenceList} from "../../../parser-node";
 import {StringBuilder} from "../../string-builder";
 import {emitTableReference} from "./table-reference";
 
 export function emitTableReferenceList (arr : TableReferenceList) {
+    if (arr.length == 1) {
+        return emitTableReference(arr[0], false);
+    }
+
     return new StringBuilder()
         .loop(
             arr,
             builder => builder.append(",").appendNewLine(),
-            (builder, tableReference) => builder
-                .appendBuilder(emitTableReference(tableReference, false))
+            (builder, tableReference) => {
+                if (tableReference.syntaxKind == SyntaxKind.Join) {
+                    builder
+                        .append("(")
+                        .indent(builder => builder
+                            .appendBuilder(emitTableReference(tableReference, false))
+                        )
+                        .appendNewLine()
+                        .append(")")
+                } else {
+                    builder
+                        .appendBuilder(emitTableReference(tableReference, false))
+                }
+            }
         )
 }
